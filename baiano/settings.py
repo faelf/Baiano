c030b4edb3bd41e8e26f9fb27fce10e2bc775207
Rafael Ferreira <rafaelf_s@hotmail.com>
2026-08-15 17:58:38 +0000

    fix: env file, remove db file from the repo, install dotenv

--- baiano/settings.py
+++ baiano/settings.py
@@ -8,17 +8,17 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
 
 import os
 import dj_database_url
 
-if os.path.isfile("env.py"):
-    import env  # noqa
+# Load environment variables from .env file
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 TEMPLATES_DIR = os.path.join(BASE_DIR, "templates")
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
@@ -28,10 +28,9 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 DEBUG = os.environ.get("DEBUG", "False") == "True"
 
 ALLOWED_HOSTS = [
-    "baiano.faelf.uk",
-    "baiano",
     "localhost",
     "127.0.0.1",
+    *os.environ.get("ALLOWED_HOSTS", "").split(","),
 ]
 
 INSTALLED_APPS = [
